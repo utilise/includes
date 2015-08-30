f5fa9d3af90b0d776f994418823c06285f351577
pemrouz <p3pedram@hotmail.com>
2015-08-30 23:01:35 +0100

    update to not fail on non sets

--- test.js
+++ test.js
@@ -2,8 +2,15 @@ var expect = require('chai').expect
   , includes = require('./')
 
 describe('includes', function() {
+
   it('should check whether datum includes pattern', function() {
     expect(includes('.js')('file.js')).to.be.ok
     expect(includes('.js')('file.txt')).to.not.be.ok
   })
+
+  it('should fail gracefull with falsy', function() {
+    expect(includes('.js')(false)).to.not.be.ok
+    expect(includes('.js')({})).to.not.be.ok
+  })
+
 })
